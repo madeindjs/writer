63cc2f82b4bb69746c3fe4cde3dc16581d34326e
madeindjs <madeindjs@gmail.com>
2016-08-29 10:55:00 +0200

    replace returns by a print()

--- writter.py
+++ writter.py
@@ -24,50 +24,48 @@ class Writter():
 		"""decorator to display this outpout only if verbose is True """
 		def wrapper(cls, *args):
 			if cls.verbose:
-				return method(cls, *args)
-		return wrapper
+				print(method(cls, *args))
+		print(wrapper)
 
 
 
 	@classmethod
 	def title(cls, title):
-		"""return a beautiful title like this
+		"""print a beautiful title like this
 
 		-------------------
 		    I'm a title
 		-------------------"""
-		return "\r\n{line}\r\n{title}\r\n{line}\r\n".format( line=cls.line(), 
-									title=Writter.center_text(title) )
+		print("\r\n{line}\r\n{title}\r\n{line}\r\n".format( line=cls.line(), 
+									title=Writter.center_text(title) ) )
 
 
 
 	@classmethod
 	def subtitle(cls, title):
-		"""return a beautiful title like this
+		"""print a beautiful title like this
 
 		I'm a sub-title
 		-------------------"""
-		return "{title}\r\n{line}".format( line=cls.line(),  title=title)
+		print("{title}\r\n{line}".format( line=cls.line(),  title=title) )
 
 
 
 	@classmethod
 	def unsorted_list(cls, *args):
-		"""return a beautiful list like this
+		"""print a beautiful list like this
 
 		-	-   I'm
 			-   an unsorted
 			-   list"""
-		output = str()
 		for arg in args:
-			output += "\t-\t{}".format(arg)
-		return output
+			print("\t-\t{}".format(arg))
 
 
 
 	@classmethod
 	def sorted_list(cls, *args):
-		"""return a beautiful list like this
+		"""print a beautiful list like this
 
 			1 - I'm
 			2 - a sorted
@@ -75,39 +73,38 @@ class Writter():
 		i = 0 
 		output = str()
 		for arg in args:
-			output += "\t{n} - {text}\r\n".format(n=i,text=arg)
+			print("\t{n} - {text}\r\n".format(n=i,text=arg))
 			i+=1
-		return output
 
 
 
 	@classmethod
 	def center_text(cls, text):
-		"""return center text on cls.width"""
+		"""print center text on cls.width"""
 		len_whitespace = int((cls.width - len(text) ) /2)
-		return '{white_space}{title}{white_space}'.format(
-			white_space=' '*len_whitespace, title=text )
+		print('{white_space}{title}{white_space}'.format(
+			white_space=' '*len_whitespace, title=text ) )
 
 
 
 	@classmethod
 	def line(cls):
-		"""return a simply line"""
-		return '-'*cls.width
+		"""print a simply line"""
+		print('-'*cls.width )
 
 
 
 	@staticmethod
 	def input(question):
 		"""just format an input()"""
-		return input('{}? ...'.format(question))
+		print(input('{}? ...'.format(question)) )
 
 
 
 
 	@staticmethod
 	def ask_int(question):
-		"""ask an integer, try to convert input and return it in integer"""
+		"""ask an integer, try to convert input and print it in integer"""
 		output = str()
 		while output.__class__ != int().__class__ :
 			try:
@@ -116,21 +113,21 @@ class Writter():
 			except ValueError:
 				output = str()
 
-		return output
+		print(output)
 			
 
 
 	@printed
 	@classmethod
 	def event(cls, text):
-		return '\r\n- {}'.format(text)
+		print('\r\n- {}'.format(text) )
 
 
 
 	@printed
 	@classmethod
 	def sql_log(cls, sql_query, data=None):
-		"""return a SQL Log"""
+		"""print a SQL Log"""
 		# if data exists , I replace them into `complete_sql_query`
 		if data:
 			for key, value in data.items():
@@ -138,6 +135,6 @@ class Writter():
 				replace = '`{}`'.format(value)
 				sql_query = sql_query.replace(search, replace)
 
-		return '\t{}'.format(sql_query)
+		print('\t{}'.format(sql_query))
 
 
